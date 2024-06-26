008255512f3d3a6768de6a78b189d5d9c15ca089
gatopotato <samkitjai09@gmail.com>
2024-06-26 12:53:39 +0530

    Add more handlers for agent route.

--- src/controllers/agent.controller.js
+++ src/controllers/agent.controller.js
@@ -2,6 +2,7 @@ import { Agent } from "../models/agent.model.js";
 import { apiResponse } from "../utils/apiresponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { RelationshipManager } from "../models/relationshipManager.model.js";
+import { Head } from "../models/head.model.js";
 import { apiError } from "../utils/apiError.js";
 const getAgents = asyncHandler(async (req, res) => {
   let data = await Agent.aggregate([
@@ -44,4 +45,32 @@ const getRequiredAgents = asyncHandler(async (req, res) => {
   return res.status(200).json(new apiResponse(200, data, "Agents returned."));
 });
 
-export { getAgents, getRequiredAgents };
+const getRequiredAgentsbyhead = asyncHandler(async (req, res) => {
+  const { headID } = req.params;
+
+  const head = await Head.findById(
+    headID
+  );
+
+  if (!head) {
+    throw new apiError(404, "Head not found.");
+  }
+
+  let data = await Agent.aggregate([
+    {
+      $match: {
+        headId: ObjectId(headID),
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  return res.status(200).json(new apiResponse(200, data, "Agents returned."));
+});
+
+export { getAgents, getRequiredAgents, getRequiredAgentsbyhead };
--- src/routes/agent.routes.js
+++ src/routes/agent.routes.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import {
   getAgents,
   getRequiredAgents,
+  getRequiredAgentsbyhead
 } from "../controllers/agent.controller.js";
 
 const router = Router();
 router.get("/", getAgents);
 router.get("/rm/:relationshipManagerID", getRequiredAgents);
-
+router.get("/head/:headID", getRequiredAgents);
 export default router;
